c4888a0100155a00ea233a6b205beda8e33a12b9
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-29 04:23:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat(array1, array2)
  array1.concat(array2)
 end
 
-def using_insert(array1)
-  using_insert.insert(array2)
+def using_insert(array1, array2)
+  using_insert.insert(array3)
   
 end
